f24f50e8b25196dccbe9727cd9c266eb86bd7933
Jacob Coons <jacobtcoons@gmail.com>
2019-08-19 23:28:00 -0700

    an efficient search DONE

---
+++ efficient-search.js
@@ -0,0 +1,30 @@
+// Q: What is the Big O of the following algorithm? Explain your answer
+
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+/*
+A: This algorithm has logarithmic (O(logn)) time complexity, because it is a characteristic
+of logarithmic algorithms to cut the problem in half each run through the function.
+
+*/
