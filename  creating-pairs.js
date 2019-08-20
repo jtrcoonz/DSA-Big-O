5fffa1282f6a225e764d0cda4bda3f7c75fadc5a
Jacob Coons <jacobtcoons@gmail.com>
2019-08-19 23:01:55 -0700

    creating pairs DONE

---
+++  creating-pairs.js
@@ -0,0 +1,11 @@
+// Q: What is the Big O of the following algorithm? Explain your answer
+
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+//A: This function has polynomial (O(n^k)) time complexity. Same reasons - nested loops.
